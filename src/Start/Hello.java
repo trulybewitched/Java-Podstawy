f3fe71c6afbbd6736fc79a51fbf73fa14625c4fc
trulybewitched <trulybewitched90@gmail.com>
2019-02-14 10:05:33 +0100

    START

---
+++ src/Start/Hello.java
@@ -0,0 +1,11 @@
+package Start;
+
+// Komentarz
+// psvm - public static void main(String[] args}
+// sout - System.out.printIn();
+// CTRL + SHIFT + F10 - run
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
